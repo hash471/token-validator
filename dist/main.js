5985e6481edc6e2ec43078027801c5104491b8f1
Sriharsha Ramachandra <harsha.471@gmail.com>
2021-09-08 16:04:16 +0530

    csrf

--- dist/main.js
+++ dist/main.js
@@ -2,11 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const csurf = require("csurf");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
-    app.use(csurf());
     await app.listen(3000);
 }
 bootstrap();
