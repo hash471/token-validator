5ed01c58a528620e2ed25abaa6a127c5726e58eb
Sriharsha Ramachandra <harsha.471@gmail.com>
2021-09-13 15:28:29 +0530

    Adding certificate

--- dist/main.js
+++ dist/main.js
@@ -2,8 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const fs = require("fs");
+const httpsOptions = {
+    key: fs.readFileSync("./secrets/key.pem", "utf8"),
+    cert: fs.readFileSync("./secrets/server.crt", "utf8"),
+};
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { httpsOptions });
     app.enableCors();
     await app.listen(3000);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync("./secrets/key.pem", "utf8"),
+  cert: fs.readFileSync("./secrets/server.crt", "utf8"),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {httpsOptions});
   app.enableCors();
   await app.listen(3000);
 }
